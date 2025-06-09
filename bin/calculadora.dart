e035686243c5300aa7b9bf39086bc63c407b15a9
felipeembersisc <felipe.embersisc@gmail.com>
2025-06-09 16:16:18 -0300

    first commit

---
+++ bin/calculadora.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync()!);
+  print(numeroUm + numeroDois);
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+}
